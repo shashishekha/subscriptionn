71bdd20d6c9c366c26e7ea0e4c926da0ea366925
Shashi Shekhar <shashiyadav7629518@gmail.com>
2026-05-30 16:31:56 +0530

    added custom middleware

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .import models
+
+admin.site.register(models.Blog)
+admin.site.register(models.Subscription)
+admin.site.register(models.SubscriptionOrder)
+
---
+++ middlewares/middleware.py
@@ -0,0 +1,37 @@
+from home.models import Subscription, SubscriptionOrder
+from rest_framework.response import Response
+from django.http import JsonResponse
+import datetime
+
+
+class SubscriptionCheck(object):
+    def __init__(self, get_response) -> None:
+        self.get_response = get_response
+
+    def __call__(self,request):
+        url_path = set((request.path).split('/'))
+        if 'blog_detail' in url_path :
+            if not request.user.is_authenticated:
+                return Response({
+                    'status': False,
+                    'message': 'You are not authenticated.',
+                    'data':{}
+                },
+                content_type = "application/json")
+
+            else:
+                subscription_order = SubscriptionOrder.objects.filter(
+                    user = request.user,
+                    subscription_end_date = datetime.datetime.today().date()
+                )
+
+                if not subscription_order.exists():
+                    return JsonResponse({
+                        'status': False,
+                        'message':'you have not purchased the subscription',
+                        'data' : {}
+                    },
+                    content_type = "application/json")
+
+
+        return self.get_response(request)
